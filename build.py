d35014113603ccd5802c0231d313a614945b3821
julien.waechter <julien.waechter@gmail.com>
2018-10-05 09:54:59 +0200

    chore(conanfile): clean gitlab-ci script

--- build.py
+++ build.py
@@ -4,7 +4,7 @@ from conans import tools
 if __name__ == "__main__":
     if tools.os_info.is_windows:
         builder = ConanMultiPackager(
-            username="fw4spl", 
+            username="sight", 
             visual_runtimes=["MD", "MDd"],
             archs=["x86_64"])
         builder.add_common_builds(shared_option_name=False, pure_c=True)
