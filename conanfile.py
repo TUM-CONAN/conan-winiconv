5770932768d630e14e5fd22fbeea9eb9336f27bb
Didier WECKMANN <didier.weckmann@ircad.fr>
2019-06-03 14:18:20 +0200

    feat(conanfile): update winiconv from 0.0.8-r1 to 0.0.8-r2

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ from conans import ConanFile, tools, CMake
 class WiniconvConan(ConanFile):
     name = "winiconv"
     upstream_version = "0.0.8"
-    package_revision = "-r1"
+    package_revision = "-r2"
     version = "{0}{1}".format(upstream_version, package_revision)
 
     generators = "cmake"
@@ -21,6 +21,9 @@ class WiniconvConan(ConanFile):
     def configure(self):
         del self.settings.compiler.libcxx
 
+    def requirements(self):
+        self.requires("common/1.0.0@sight/stable")
+
     def source(self):
         winiconv_name = "winiconv-%s.tar.gz" % self.upstream_version
         tools.download("https://github.com/win-iconv/win-iconv/archive/v{0}.tar.gz".format(self.upstream_version), winiconv_name)
@@ -28,8 +31,15 @@ class WiniconvConan(ConanFile):
         os.unlink(winiconv_name)
 
     def build(self):
+        #Import common flags and defines
+        import common
         iconv_source_dir = os.path.join(self.source_folder, "win-iconv-{0}".format(self.upstream_version))
         cmake = CMake(self)
+        
+        #Set common flags
+        cmake.definitions["CMAKE_C_FLAGS"] = common.get_c_flags()
+        cmake.definitions["CMAKE_CXX_FLAGS"] = common.get_cxx_flags()
+        
         cmake.configure(source_folder=iconv_source_dir)
         cmake.build()
         cmake.install()
